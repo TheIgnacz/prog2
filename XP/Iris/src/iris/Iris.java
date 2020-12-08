696d93fc751c34e5507a5f4d75b68bbc961d374e
TheIgnacz <55457723+TheIgnacz@users.noreply.github.com>
2020-12-08 23:05:41 +0100

    Add files via upload

--- XP/Iris/src/iris/Iris.java
+++ XP/Iris/src/iris/Iris.java
@@ -1,6 +1,19 @@
 package iris;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Iris {
+
+    public static final String IRIS_SETOSA = "Iris-setosa";
+    public static final String IRIS_VERSICOLOR = "Iris-versicolor";
+    public static final String IRIS_VIRGINICA = "Iris-virginica";
+
     double petalWidth;
     double septalWidth;
     double petalHeight;
@@ -24,5 +37,56 @@ public class Iris {
                 ", septalHeight=" + septalHeight +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    public static void read(File myFile, ArrayList<Iris> irisList) throws FileNotFoundException {
+        Scanner s = new Scanner(new FileReader(myFile));
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            String[] unit = line.split(",");
+            irisList.add(new Iris(Double.valueOf(unit[0]), Double.valueOf(unit[1]), Double.valueOf(unit[2]),Double.valueOf(unit[3]), unit[4]));
+            }
+    }
+
+    public static double count(ArrayList<Iris> irisList, String name) {
+        double counter = 0;
+        for ( Iris i : irisList) {
+            if ( i.name.contains(name)) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public static double[] sum(ArrayList<Iris> irisList, String name) {
+        double[] data = new double[4]; //0: PetalWidth, 1: septalWidth, 2: petalHeight, 3: septalHeight
+        for ( Iris i : irisList) {
+            if ( i.name.contains(name)) {
+                data[0] += i.petalWidth;
+                data[1] += i.septalWidth;
+                data[2] += i.petalHeight;
+                data[3] += i.septalHeight;
+            }
+        }
+        return data;
+    }
+
+    public static void calculate(ArrayList<Iris> irisList) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        printFlowerType(irisList, df, IRIS_SETOSA);
+        printFlowerType(irisList, df, IRIS_VERSICOLOR);
+        printFlowerType(irisList, df, IRIS_VIRGINICA);
+    }
+
+    private static void printFlowerType(ArrayList<Iris> irisList, DecimalFormat df, String irisType) {
+        double[] sumArray = sum(irisList, irisType);
+        double count = count(irisList, irisType);
+        System.out.println(df.format(sumArray[0]/count) + " " +
+                df.format(sumArray[1]/count) + " " +
+                df.format(sumArray[2]/count) + " " +
+                df.format(sumArray[3]/count));
     }
 }
--- XP/Iris/src/iris/Main.java
+++ XP/Iris/src/iris/Main.java
@@ -2,86 +2,26 @@ package iris;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        File myFile = new File("iris.data");
-        ArrayList<Iris> iris = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        File myFile = null;
+        if (args.length > 0) {
+            myFile = new File(args[0]);
+        } else {
+            Scanner scanner = new Scanner(System.in);
 
-        try (Scanner s = new Scanner(new FileReader(myFile))) {
-            while (s.hasNext()) {
-                String line = s.nextLine();
-                String[] unit = line.split(",");
-                iris.add(new Iris(Double.valueOf(unit[0]), Double.valueOf(unit[1]), Double.valueOf(unit[2]),Double.valueOf(unit[3]), unit[4]));
-                //System.out.println(s.nextLine());
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("Enter filename you wish to process:");
+            String fileName = scanner.nextLine();
+            myFile = new File(fileName);
         }
 
-        int setosaCount = 0;
-        double setosaPetalWidth = 0;
-        double septalWidth = 0;
-        double setosaPetalHeight = 0;
-        double septalHeight = 0;
+        ArrayList<Iris> irisList = new ArrayList<>();
 
-        int versicolorCount = 0;
-        double versicolorPetalWidth = 0;
-        double versicolorWidth = 0;
-        double versicolorPetalHeight = 0;
-        double versicolorHeight = 0;
-
-        int virginicalCount = 0;
-        double virginicaPetalWidth = 0;
-        double virginicaWidth = 0;
-        double virginicaPetalHeight = 0;
-        double virginicaHeight = 0;
-
-        for( Iris i : iris) {
-            switch(i.name) {
-                case "Iris-setosa":
-                    setosaCount++;
-                case "Iris-versicolor":
-                    versicolorCount++;
-                case "Iris-virginica":
-                    virginicalCount++;
-            }
-        }
-
-        for( Iris i : iris) {
-            switch(i.name) {
-                case "Iris-setosa":
-                    setosaPetalWidth += i.petalWidth;
-                    septalWidth += i.septalWidth;
-                    setosaPetalHeight += i.petalHeight;
-                    septalHeight += i.septalHeight;
-                case "Iris-versicolor":
-                    versicolorPetalWidth += i.petalWidth;
-                    versicolorWidth += i.septalWidth;
-                    versicolorPetalHeight += i.petalHeight;
-                    versicolorHeight += i.septalHeight;
-                case "Iris-virginica":
-                    virginicaPetalWidth += i.petalWidth;
-                    virginicaWidth += i.septalWidth;
-                    virginicaPetalHeight += i.petalHeight;
-                    virginicaHeight += i.septalHeight;
-            }
-        }
-
-        DecimalFormat df = new DecimalFormat("#.##");
-        df.setRoundingMode(RoundingMode.CEILING);
-
-        System.out.println(df.format(setosaPetalWidth/setosaCount) + " " + df.format(septalWidth/setosaCount) + " " + df.format(setosaPetalHeight/setosaCount) + " " + df.format(septalHeight/setosaCount));
-        System.out.println(df.format(versicolorPetalWidth/versicolorCount) + " " + df.format(versicolorWidth/versicolorCount) + " " + df.format(versicolorPetalHeight/versicolorCount) + " " + df.format(versicolorHeight/versicolorCount));
-        System.out.println(df.format(virginicaPetalWidth/virginicalCount) + " " + df.format(virginicaWidth/virginicalCount) + " " + df.format(virginicaPetalHeight/virginicalCount) + " " + df.format(virginicaHeight/virginicalCount));
-
-
+        Iris.read(myFile, irisList);
+        Iris.calculate(irisList);
     }
 }
